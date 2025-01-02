8774e3c18b249f320ea366e15334672c98495bee
Armando Sanchez <deathbat@gmail.com>
2025-01-02 00:51:38 -0800

    fixing blank text issue

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './App.css';
 
 const App = () => {
     return (
         <div>
-            <h1>Hello World! GitHub co-pilot is my friend.</h1>
+            <h1>Hello World! GitHub Copilot is my friend.</h1>
             <p>Blah blah blah, something something.</p>
         </div>
     );
